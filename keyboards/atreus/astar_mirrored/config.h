f1d2ef7d40f80730a3c302ee3d775a06a6c8f88a
nick pascucci <nick.pascucci@spire.com>
2021-10-14 10:42:57 +0100

    Add tap-hold config to Atreus

--- keyboards/atreus/astar_mirrored/config.h
+++ keyboards/atreus/astar_mirrored/config.h
@@ -17,3 +17,10 @@
 #pragma once
 
 #define PCBDOWN 1
+
+#define MATRIX_ROW_PINS { D0, D1, D3, D2 }
+#define MATRIX_COL_PINS { B7, D6, F7, F6, B6, D4, E6, B4, B5, C6, D7 }
+#define UNUSED_PINS
+
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
