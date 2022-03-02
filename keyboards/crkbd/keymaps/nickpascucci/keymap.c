6a7a239850fafad6bc608bc633476a00343ed681
nick pascucci <nick.pascucci@spire.com>
2022-03-02 21:18:25 +0000

    fix: remove stray comma

--- keyboards/crkbd/keymaps/nickpascucci/keymap.c
+++ keyboards/crkbd/keymaps/nickpascucci/keymap.c
@@ -186,7 +186,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX, KC_ACL0, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                          KC_BTN3, KC_BTN1, KC_BTN2,    XXXXXXX, XXXXXXX, TO(L_COLE),
+                                          KC_BTN3, KC_BTN1, KC_BTN2,    XXXXXXX, XXXXXXX, TO(L_COLE)
                                       //|--------+--------+--------|  |--------+--------+--------|
   ),
 
