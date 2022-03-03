35c69775e8671a5e896d21071a141233eb7b0478
nick pascucci <nick.pascucci@spire.com>
2022-03-03 16:16:37 +0000

    feat: lower tapping term to help with wanted mod activation

--- keyboards/crkbd/keymaps/nickpascucci/config.h
+++ keyboards/crkbd/keymaps/nickpascucci/config.h
@@ -32,7 +32,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 // #define PERMISSIVE_HOLD
 #define IGNORE_MOD_TAP_INTERRUPT
-#define TAPPING_TERM 200
+#define TAPPING_TERM 180
 #define ONESHOT_TAP_TOGGLE 2
 
 #define BILATERAL_COMBINATIONS
