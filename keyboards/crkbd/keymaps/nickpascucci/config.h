e63d179fe343a958642e900f68ce39e07fbb520c
Nick Pascucci <ndpi@alum.mit.edu>
2023-09-08 13:39:05 +0100

    fix: disable permissive_hold again, it was causing accidental mods

--- keyboards/crkbd/keymaps/nickpascucci/config.h
+++ keyboards/crkbd/keymaps/nickpascucci/config.h
@@ -30,7 +30,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define USE_SERIAL_PD2
 
-#define PERMISSIVE_HOLD
+// #define PERMISSIVE_HOLD
 #define TAPPING_TERM 190
 #define ONESHOT_TAP_TOGGLE 2
 
